6e119052a7b0467789faf8dc5226d27980729a5e
“Johnatan <jonystrit@gmail.com>
2021-12-12 22:01:07 -0300

    Criando player de musica com HTML, CSS e JS

---
+++ script.js
@@ -0,0 +1,84 @@
+//player
+let music = document.querySelector(".music-element")
+let playBtn = document.querySelector(".play")
+let seekbar = document.querySelector(".seekbar")
+let currentTime = document.querySelector(".current-time")
+let duration = document.querySelector(".duration")
+
+function handlePlay(){
+    if (music.paused){
+        music.play()
+        playBtn.className = 'pause'
+        playBtn.innerHTML = '<i class="fas fa-pause"></i>'
+    }else{
+        music.pause()
+        playBtn.className = 'play'
+        playBtn.innerHTML = '<i class="fas fa-play"></i>'
+    }
+    music.addEventListener('ended', function(){
+        playBtn.className = 'play'
+        playBtn.innerHTML = '<i class="fas fa-play"></i>s'
+        music.currentTime = 0
+    })
+}
+
+music.onloadeddata = function(){
+    seekbar.max = music.duration
+    let ds = parseInt(music.duration%60)
+    let dm = parseInt((music.duration/60)%60)
+    duration.innerHTML = dm + ':' + ds
+}
+
+music.ontimeupdate = ()=>{seekbar.value = music.currentTime}
+
+handleSeekBar = ()=>{music.currentTime = seekbar.value}
+music.addEventListener('timeupdate', ()=>{
+    let cs = parseInt(music.currentTime%60)
+    let cm = parseInt((music.currentTime/60)%60)
+    currentTime.innerHTML = cm + ':' + cs
+},false)
+
+//like
+
+let favIcon = document.querySelector(".favorite")
+function handleFavorite(){
+    favIcon.classList.toggle('active')
+}
+
+//repeat
+
+let repIcon = document.querySelector(".repeat")
+function handleRepeat(){
+    if(music.loop){
+        music.loop = false
+        repIcon.classList.toggle('active')
+    }else{
+        music.loop = true
+        repIcon.classList.toggle('active')
+    }
+}
+
+//volume
+
+var volIcon = document.querySelector('.volume')
+var volBox = document.querySelector('.volume-box')
+var volumeRange = document.querySelector('.volume-range')
+var volumeDown = document.querySelector('.volume-down')
+var volumeUp = document.querySelector('.volume-up')
+
+function handleVolume() {
+    volIcon.classList.toggle('active')
+    volBox.classList.toggle('active')
+}
+
+volumeDown.addEventListener('click', handleVolumeDown);
+volumeUp.addEventListener('click', handleVolumeUp);
+
+function handleVolumeDown() {
+    volumeRange.value = Number(volumeRange.value) - 10
+    music.volume = volumeRange.value / 100
+}
+function handleVolumeUp() {
+    volumeRange.value = Number(volumeRange.value) + 10
+    music.volume = volumeRange.value / 100
+}
